ce5264f04487ddd1528e1a61f588340a62fe6419
Hans Yu <hansyuhans@gmail.com>
2019-04-02 22:08:45 -0400

    Updates

---
+++ precache-manifest.a77bf5aff2e7032e76f3b9f5b8929f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83ac1018b5883472ce39",
+    "url": "/taleweaver/static/js/runtime~main.2f6baeca.js"
+  },
+  {
+    "revision": "939757baf43eb2edecae",
+    "url": "/taleweaver/static/js/main.156a1257.chunk.js"
+  },
+  {
+    "revision": "f9cbfd4ba5d2eecada66",
+    "url": "/taleweaver/static/js/2.2fb8fb8d.chunk.js"
+  },
+  {
+    "revision": "939757baf43eb2edecae",
+    "url": "/taleweaver/static/css/main.2054c7e7.chunk.css"
+  },
+  {
+    "revision": "f812f7512ec8bf67e4d680dac8b75770",
+    "url": "/taleweaver/index.html"
+  }
+];
