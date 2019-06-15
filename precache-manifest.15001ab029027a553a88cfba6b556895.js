4b96e33bd064caac0b7ed5dea56cf3627c3a84e5
Hans Yu <hansyuhans@gmail.com>
2019-06-15 15:37:16 -0400

    Updates

--- precache-manifest.15001ab029027a553a88cfba6b556895.js
+++ precache-manifest.15001ab029027a553a88cfba6b556895.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/taleweaver/static/js/runtime~main.2f6baeca.js"
   },
   {
-    "revision": "84e879af4d35b50ad344",
-    "url": "/taleweaver/static/js/main.b7289b49.chunk.js"
+    "revision": "69e5b85c5af19a5fe7ce",
+    "url": "/taleweaver/static/js/main.e9aabe62.chunk.js"
   },
   {
     "revision": "e80c4a2042e8feea5482",
     "url": "/taleweaver/static/js/2.5bd5c99a.chunk.js"
   },
   {
-    "revision": "84e879af4d35b50ad344",
+    "revision": "69e5b85c5af19a5fe7ce",
     "url": "/taleweaver/static/css/main.5636bf1c.chunk.css"
   },
   {
-    "revision": "a6142d9db794908a9f05e4486ed27342",
+    "revision": "a616b2fde1e671ae98fe2c71bc5acfe4",
     "url": "/taleweaver/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/taleweaver/precache-manifest.89121df61cd1249222272e095af56c7f.js"
+  "/taleweaver/precache-manifest.15001ab029027a553a88cfba6b556895.js"
 );
 
 workbox.clientsClaim();
