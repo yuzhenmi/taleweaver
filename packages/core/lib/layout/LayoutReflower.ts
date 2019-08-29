3de186e01d1a7411e879f4e286f489ddce1c8eb8
Smillence <jwsun@eiduo.com>
2019-08-29 17:40:15 +0800

    Fix breakPageNode issue (#73)

--- packages/core/lib/layout/LayoutReflower.ts
+++ packages/core/lib/layout/LayoutReflower.ts
@@ -319,7 +319,8 @@ export default class LayoutReflower {
         let cumulatedHeight = 0;
         for (let n = 0, nn = blockNodes.length; n < nn; n++) {
             const blockNode = blockNodes[n];
-            if (cumulatedHeight + blockNode.getHeight() > height) {
+            // padding-bottom 超出页面不算溢出，like ms word
+            if (cumulatedHeight + (blockNode.getHeight() - blockNode.getPaddingBottom()) > height) {
                 const newPageNode = pageNode.splitAt(n + 1);
                 const blockNodes = pageNode.getChildNodes();
                 const trailingBlockNode = blockNodes[blockNodes.length - 1];
