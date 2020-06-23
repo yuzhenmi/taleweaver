691005d9d68fd94bc23f41f5cb9565b087c27903
Hans Yu <hansyuhans@gmail.com>
2020-06-22 21:59:54 -0400

    Updates

---
+++ precache-manifest.3773c26cdc90003c1ff50fe0b9bb1aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fa727e7922faa753e9deea79b8762b",
+    "url": "/taleweaver/index.html"
+  },
+  {
+    "revision": "6cd48ccc8288800c0749",
+    "url": "/taleweaver/static/css/main.6c23ad2e.chunk.css"
+  },
+  {
+    "revision": "c8d3b5aa0271cd5065e0",
+    "url": "/taleweaver/static/js/2.d501ae8d.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/taleweaver/static/js/2.d501ae8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd48ccc8288800c0749",
+    "url": "/taleweaver/static/js/main.8a9ade95.chunk.js"
+  },
+  {
+    "revision": "0c3df9d6070c5ed2eed6",
+    "url": "/taleweaver/static/js/runtime-main.84869db2.js"
+  }
+]);
